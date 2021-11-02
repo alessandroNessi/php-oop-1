4a2cebbadadfaf38d3e435cee44047163a979deb
alessandroNessi <alessandronessi@msn.com>
2021-11-02 15:59:52 +0100

    aggiunto funzione print

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php 
+    function printMovie($movie){
+        echo("
+        <h2>{$movie->getName()}</h2>
+        <p>{$movie->getDate()}</p>
+        <p>{$movie->getDirector()}</p>
+        <p>{$movie->getGenres()}</p>
+        <p>{$movie->getDescription()}</p>
+        ");
+    }
     require __DIR__.('/movie.php');
     $movie1=new Movie('La notte dei morti morenti','01/01/2001','Cippa Lippa',['horror','grottesco','comico'],'i morti muoiono e rimangono tali, fine. loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem');
-    var_dump($movie1);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php echo printMovie($movie1); ?>
+    </div>
+</body>
+</html>
--- movie.php
+++ movie.php
@@ -12,25 +12,40 @@
                 $this->name=$_name;
             }
         }
+        public function getName(){
+            return $this->name;
+        }
         public function setDate($_releaseDate){
             if(strlen($_releaseDate)==10){
                 $this->releaseDate=$_releaseDate;
             }
+        }
+        public function getDate(){
+            return $this->releaseDate;
         }
         public function setDirector($_director){
             if(strlen($_director)<50){
                 $this->director=$_director;
             }
         }
+        public function getDirector(){
+            return $this->director;
+        }
         public function setGenres($_genres){
             if(strlen($_genres)<80){
                 $this->genres=$_genres;
             }
         }
+        public function getGenres(){
+            return $this->genres;
+        }
         public function setDescription($_description){
             if(strlen($_description)<250){
                 $this->description=$_description;
             }
+        }
+        public function getDescription(){
+            return $this->description;
         }
         public function __construct($_name,$_releaseDate,$_director,$_genres,$_description){
             $this->setName($_name);
